416fae34402fcca9b4d10efa0bc2db8214fc4ba7
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-10-19 21:51:39 +0200

    setting number of tiles

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -3,15 +3,18 @@ class Game {
         this.boardSize = document.querySelector('.start-window__tile-number');
         this.numberOfTiles = 0;
         this.playButton = document.querySelector('.start-window__button');
+
+        this.playButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.setBoardSize();
+        })
     }
 
     setBoardSize = () => {
-
+        this.numberOfTiles = this.boardSize.value * 2;
+        console.log(this.numberOfTiles);
     }
 
-    test = () => {
-        console.log('from test')
-    }
 }
 
 const game = new Game();
