c22d324ad32afd39023cd4d243efcb22c4df10c3
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-10-22 22:10:14 +0200

    Adding img to array

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -1,20 +1,54 @@
 class Game {
     constructor() {
+
+        let listOfImages = [];
+
+        this.urlApi = "https://dog.ceo/api";
         this.boardSize = document.querySelector('.start-window__tile-number');
         this.numberOfTiles = 0;
         this.playButton = document.querySelector('.start-window__button');
+        this.tileTest = document.querySelector('.tileTest');
 
         this.playButton.addEventListener('click', (e) => {
             e.preventDefault();
             this.setBoardSize();
+            console.log(this.numberOfTiles);
+            for (let i = 0; i < this.numberOfTiles; i++) {
+                this.getImage()
+                    .then(img => {
+                        listOfImages.push(img);
+                        listOfImages.push(img);
+                        console.log(listOfImages)
+                    })
+            }
+            this.getImage()
+                .then(image => {
+                    this.setImage(image)
+                })
         })
     }
 
     setBoardSize = () => {
         this.numberOfTiles = this.boardSize.value * 2;
-        console.log(this.numberOfTiles);
+    }
+
+    getImage = () => {
+        return fetch(`${this.urlApi}/breeds/image/random`)
+            .then(response => response.json())
+            .then(image => image.message)
+    }
+
+    setImage = (image) => {
+        this.tileTest.style.backgroundImage = `url("${image}")`;
+    }
+
+    createTile = () => {
+
     }
 
 }
+
+
+
 
 const game = new Game();
