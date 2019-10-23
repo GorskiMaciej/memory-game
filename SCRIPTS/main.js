a3ccd30da6e54830858f0be4dd52650cd51300b2
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-10-23 23:00:58 +0200

    adding fetched images to array

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -5,22 +5,15 @@ class Game {
 
         this.urlApi = "https://dog.ceo/api";
         this.boardSize = document.querySelector('.start-window__tile-number');
-        this.numberOfTiles = 0;
+        this.numberOfPairs = 0;
         this.playButton = document.querySelector('.start-window__button');
         this.tileTest = document.querySelector('.tileTest');
 
         this.playButton.addEventListener('click', (e) => {
             e.preventDefault();
             this.setBoardSize();
-            console.log(this.numberOfTiles);
-            for (let i = 0; i < this.numberOfTiles; i++) {
-                this.getImage()
-                    .then(img => {
-                        listOfImages.push(img);
-                        listOfImages.push(img);
-                        console.log(listOfImages)
-                    })
-            }
+            console.log("Number of tiles: " + this.numberOfTiles);
+            this.createBoard(listOfImages);
             this.getImage()
                 .then(image => {
                     this.setImage(image)
@@ -30,6 +23,7 @@ class Game {
 
     setBoardSize = () => {
         this.numberOfTiles = this.boardSize.value * 2;
+        this.numberOfPairs = this.boardSize.value;
     }
 
     getImage = () => {
@@ -42,10 +36,34 @@ class Game {
         this.tileTest.style.backgroundImage = `url("${image}")`;
     }
 
-    createTile = () => {
-
+    createTile = (imageUrl, indexOfPair) => {
+        const tile = document.createElement('div');
+        tile.setAttribute()
     }
 
+    shuffleArray = () => {}
+
+    createBoard = (listOfImages) => {
+        let indexOfPair = 0;
+        for (let i = 0; i < this.numberOfPairs; i++) {
+            this.getImage()
+                .then(img => {
+                    const pair = new Pair(img, indexOfPair)
+                    listOfImages.push(pair);
+                    listOfImages.push(pair);
+                    indexOfPair++;
+                    return listOfImages;
+                })
+                .then(promise => console.log(promise))
+        }
+    }
+
+}
+class Pair {
+    constructor(url, index) {
+        this.url = url
+        this.index = index;
+    }
 }
 
 
